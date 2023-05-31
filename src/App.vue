53c3aefd74ac88c2c24635fa2e6b8afd9e562ad5
irvanshandika <shandikamuhammadirvan@gmail>
2023-05-31 21:00:09 +0700

    Landing page with bootstrap 5.3.0 and vue js

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <header class="px-5 py-1">
+    <Navbar />
+  </header>
+
+  <main>
+    <Hero/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
